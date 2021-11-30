b723c83dd19393d1787126bd11045c7264579cab
B4uka <52460394+B4uka@users.noreply.github.com>
2021-11-30 18:49:33 +0100

    dodano HttpClient i asynchroniczne pobieranie informacji o streamerach (#4)

--- src/main/java/pl/chessWebApp/chess/controller/LichessApiController.java
+++ src/main/java/pl/chessWebApp/chess/controller/LichessApiController.java
@@ -4,21 +4,25 @@ import com.google.gson.Gson;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.chessWebApp.chess.liveStreamerLichess.StreamersLichess;
+import pl.chessWebApp.chess.utils.JsonUtils;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.util.Arrays;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.*;
+import java.util.concurrent.ExecutionException;
 
 @RestController
 public class LichessApiController {
 
     @GetMapping("/streamer")
-    public StreamersLichess[] getTopStreamers() throws IOException {
-        URL url = new URL("https://lichess.org/streamer/live");
-        InputStreamReader reader = new InputStreamReader(url.openStream());
-        StreamersLichess[] allStreamersFromLichess = new Gson().fromJson(reader, StreamersLichess[].class);
+    public StreamersLichess[] getTopStreamers () throws URISyntaxException, ExecutionException, InterruptedException {
+        JsonUtils jsonUtils = new JsonUtils();
+
+        String gsonString = jsonUtils.JSONBody(new URI("https://lichess.org/streamer/live"));
+        StreamersLichess[] allStreamersFromLichess = new Gson().fromJson(gsonString, StreamersLichess[].class);
 
         return Arrays.copyOf(allStreamersFromLichess, 10);
     }
+
+
 }
